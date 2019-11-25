02f9d63f5453882aaf478dcdf6715513b397f83d
Naveen <catchspider2002@gmail.com>
2019-11-26 01:32:25 +0530

    Moved data to single component chart

--- src/Dashboard.svelte
+++ src/Dashboard.svelte
@@ -1,9 +1,5 @@
 <script>
-  // import ChartBar from "./Templates/Organisms/Molecules/Atoms/ChartBar.svelte";
-  import ChartBarStacked from "./Templates/Organisms/Molecules/Atoms/ChartBarStacked.svelte";
-  import ChartBarGrouped from "./Templates/Organisms/Molecules/Atoms/ChartBarGrouped.svelte";
   import ChartLine from "./Templates/Organisms/Molecules/Atoms/ChartLine.svelte";
-  import ChartPie from "./Templates/Organisms/Molecules/Atoms/ChartPie.svelte";
 
   // import Timeline from "./Timeline.svelte";
   import Chart from "chart.js";
@@ -15,14 +11,26 @@
 
 <div class="bg-gray-400 w-full h-64 flex flex-wrap text-center px-2">
   <div class="bg-gray-400 flex w-full lg:flex-1">
-    <div class="bg-gray-200 w-1/2 m-2 border-t-4 border-green-400 rounded">
-      <ChartBarStacked />
+    <div class="bg-gray-200 w-1/2 m-2 border-t-4 border-green-400 rounded" />
+    <div class="bg-gray-200 w-1/2 m-2 border-t-4 border-red-500 rounded">
+      <ChartLine
+        type="bar"
+        barType="stacked"
+        beginYZero={true}
+        showLegend={true}
+        background="dark" />
     </div>
-    <div class="bg-gray-200 w-1/2 m-2 border-t-4 border-red-500 rounded" />
   </div>
   <div class="bg-gray-400 flex w-full lg:flex-1">
     <div class="bg-gray-200 flex-1 m-2 border-t-4 border-blue-400 rounded">
-      <ChartBarGrouped />
+
+      <ChartLine
+        type="bar"
+        barType="grouped"
+        barPercent="60"
+        beginYZero={true}
+        showLegend="true"
+        background="dark" />
     </div>
     <div class="bg-gray-200 flex-1 m-2 border-t-4 border-purple-400 rounded" />
   </div>
@@ -31,20 +39,19 @@
   <div class="bg-green-600 w-full lg:flex-1 m-2 rounded">
     <ChartLine
       type="line"
-      hideYGrid="true"
-      hideYLabel="true"
-      hideLegend="true"
       lineColor="white"
       pointColor="rgb(56, 161, 105)"
       backgroundColor="rgba(255, 255, 255, 0.2)"
       labelColor="white"
       gridColor="#2d3748"
-      lineArea="true"
-      lineCurve="true"/>
+      lineArea={true}
+      lineCurve={true} />
   </div>
   <div class="bg-gray-800 w-full lg:flex-1 m-2 rounded">
     <ChartLine
-      type="horizontalBar"
+      type="line"
+      beginYZero={true}
+      showXLabel={true}
       lineColor="rgb(56, 161, 105)"
       pointColor="#2d3748"
       backgroundColor="rgba(56, 161, 105, 0.2)"
@@ -54,10 +61,6 @@
   <div class="bg-gray-100 w-full lg:flex-1 m-2 rounded">
     <ChartLine
       type="line"
-      hideYGrid="true"
-      hideYLabel="true"
-      hideXLabel="true"
-      hideLegend="true"
       lineColor="rgb(56, 161, 105)"
       pointColor="white"
       backgroundColor="rgba(56, 161, 105, 0.2)"
@@ -67,12 +70,9 @@
 </div>
 <div class="bg-gray-400 w-full h-64 flex flex-wrap text-center px-2">
   <div class="bg-gray-800 w-full lg:flex-1 m-2 rounded">
-    <ChartPie
+    <ChartLine
       type="pie"
-      hideXGrid="true"
-      hideYGrid="true"
-      hideYLabel="true"
-      hideXLabel="true"
+      showLegend={true}
       lineColor="rgb(56, 161, 105)"
       borderColor="#2d3748"
       backgroundColor="rgba(56, 161, 105, 0.2)"
@@ -80,12 +80,9 @@
       gridColor="#000" />
   </div>
   <div class="bg-gray-100 w-full lg:flex-1 m-2 rounded">
-    <ChartPie
+    <ChartLine
       type="pie"
-      hideXGrid="true"
-      hideYGrid="true"
-      hideYLabel="true"
-      hideXLabel="true"
+      showLegend={true}
       lineColor="rgb(56, 161, 105)"
       borderColor="white"
       backgroundColor="rgba(56, 161, 105, 0.2)"
@@ -95,12 +92,10 @@
 </div>
 <div class="bg-gray-400 w-full h-64 flex flex-wrap text-center px-2">
   <div class="bg-gray-800 w-full lg:flex-1 m-2 rounded">
-    <ChartPie
+    <ChartLine
       type="doughnut"
-      hideXGrid="true"
-      hideYGrid="true"
-      hideYLabel="true"
-      hideXLabel="true"
+      showLegend={true}
+      donutPercent="80"
       lineColor="rgb(56, 161, 105)"
       borderColor="#2d3748"
       backgroundColor="rgba(56, 161, 105, 0.2)"
@@ -108,12 +103,9 @@
       gridColor="#000" />
   </div>
   <div class="bg-gray-100 w-full lg:flex-1 m-2 rounded">
-    <ChartPie
+    <ChartLine
       type="doughnut"
-      hideXGrid="true"
-      hideYGrid="true"
-      hideYLabel="true"
-      hideXLabel="true"
+      showLegend={true}
       lineColor="rgb(56, 161, 105)"
       borderColor="white"
       backgroundColor="rgba(56, 161, 105, 0.2)"
@@ -122,35 +114,20 @@
   </div>
 </div>
 <div class="bg-gray-400 w-full h-64 flex flex-wrap text-center px-2">
-  <div class="bg-green-600 w-full lg:flex-1 m-2 rounded">
+  <div class="bg-purple-600 w-full lg:flex-1 m-2 rounded">
     <ChartLine
       type="bar"
-      hideYGrid="true"
-      hideYLabel="true"
-      hideLegend="true"
+      background="green"
+      beginYZero={true}
       lineColor="white"
       pointColor="white"
       labelColor="white"
       gridColor="#2d3748" />
   </div>
   <div class="bg-gray-800 w-full lg:flex-1 m-2 rounded">
-    <ChartLine
-      type="bar"
-      lineColor="#51C572"
-      pointColor="lightblue"
-      labelColor="lightblue"
-      gridColor="#000" />
+    <ChartLine type="bar" background="dark" beginYZero={true} />
   </div>
   <div class="bg-gray-100 w-full lg:flex-1 m-2 rounded">
-    <ChartLine
-      type="bar"
-      hideYGrid="true"
-      hideYLabel="true"
-      hideXLabel="true"
-      hideLegend="true"
-      lineColor="#51c572"
-      pointColor="blue"
-      labelColor="blue"
-      gridColor="transparent" />
+    <ChartLine type="bar" background="light" beginYZero={true} />
   </div>
 </div>
--- src/Templates/Organisms/Molecules/Atoms/ChartLine.svelte
+++ src/Templates/Organisms/Molecules/Atoms/ChartLine.svelte
@@ -1,7 +1,37 @@
 <script>
   import { onMount, afterUpdate } from "svelte";
-  export let type, hideXGrid, hideYGrid, hideXLabel, hideYLabel, hideLegend;
-  export let lineColor, pointColor, backgroundColor, labelColor, gridColor;
+  export let type,
+    showXGrid,
+    showYGrid,
+    showXLabel,
+    showYLabel,
+    showLegend,
+    beginXZero,
+    beginYZero,
+    barType,
+    donutPercent = type == "doughnut" ? 50 : 0,
+    barPercent = 30;
+  export let background,
+    lineColor,
+    pointColor,
+    backgroundColor,
+    labelColor,
+    gridColor,
+    borderColor;
+
+  let themeColor = "#805ad5"; // purple-600
+
+  if (background == "dark") {
+    lineColor = themeColor;
+    pointColor = "lightblue";
+    labelColor = "red";
+    gridColor = "#000";
+  } else if (background == "light") {
+    lineColor = themeColor;
+    pointColor = "blue";
+    labelColor = "blue";
+    gridColor = "transparent";
+  }
 
   export let lineArea, lineCurve;
   let chartId = "random" + Math.floor(Math.random() * 10000 + 1);
@@ -10,8 +40,8 @@
     {
       label: "# of Votes",
       data: [12, 19, 3, 5, 2, 3, 4],
-      lineTension: lineCurve? 0.4 : 0,
-      fill: lineArea ? true : false,
+      lineTension: lineCurve ? 0.4 : 0,
+      fill: lineArea,
       backgroundColor: backgroundColor,
       borderColor: lineColor,
       borderWidth: 3,
@@ -21,12 +51,47 @@
   ];
 
   if (type == "bar" || type == "horizontalBar") {
+    if (barType == "stacked" || barType == "grouped") {
+      dataset = [
+        {
+          label: "New Clients",
+          backgroundColor: "#9f7aea",
+          data: [664, 691, 636, 662, 686, 668, 622],
+          barPercentage: barPercent / 100
+        },
+        {
+          label: "Retained Clients",
+          backgroundColor: "#f56565",
+          data: [106, 131, 156, 184, 186, 142, 124],
+          barPercentage: barPercent / 100
+        }
+      ];
+    } else {
+      dataset = [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3, 4],
+          backgroundColor: lineColor,
+          barPercentage: barPercent / 100
+        }
+      ];
+    }
+  } else if (type == "pie" || type == "doughnut") {
     dataset = [
       {
         label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3, 4],
-        backgroundColor: lineColor,
-        barPercentage: 0.3
+        data: [12, 19, 3, 5, 2, 3, 5],
+        backgroundColor: [
+          "#38a169",
+          "#e53e3e",
+          "#d69e2e",
+          "#319795",
+          "#3182ce",
+          "#805ad5",
+          "#d000d5"
+        ],
+        borderColor: borderColor,
+        borderWidth: 3
       }
     ];
   }
@@ -52,29 +117,32 @@
         maintainAspectRatio: false,
         showScale: true,
         animateScale: true,
+        cutoutPercentage: donutPercent,
         scales: {
           yAxes: [
             {
+              stacked: barType == "stacked",
               ticks: {
-                beginAtZero: true,
+                beginAtZero: beginYZero,
                 fontColor: labelColor,
-                display: hideYLabel ? false : true
+                display: showYLabel
               },
               gridLines: {
-                display: hideYGrid ? false : true,
+                display: showYGrid,
                 color: gridColor
               }
             }
           ],
           xAxes: [
             {
+              stacked: barType == "stacked",
               ticks: {
-                beginAtZero: true,
+                beginAtZero: beginXZero,
                 fontColor: labelColor,
-                display: hideXLabel ? false : true
+                display: showXLabel
               },
               gridLines: {
-                display: hideXGrid ? false : true,
+                display: showXGrid,
                 color: gridColor,
                 zeroLineColor: gridColor
               }
@@ -84,9 +152,12 @@
         legend: {
           labels: {
             fontColor: labelColor,
-            display: hideLegend ? false : true
+            display: showLegend
           },
-          display: false
+          display: showLegend
+        },
+        tooltips: {
+          mode: barType == "stacked" ? "index" : "nearest"
         }
       }
     });
